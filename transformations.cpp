bd5016a5e78b8022aa23326c5c0e9673f86c645c
Aadarsha Dhakal <braveboy2058@gmail.com>
2023-05-03 21:06:48 +0545

    2D Transformations Lab 4

--- transformations.cpp
+++ transformations.cpp
@@ -6,6 +6,27 @@
 typedef Eigen::Matrix<float, 3, 3> Matrix3f;
 typedef Eigen::Matrix<float, 3, 1> Vector3f;
 
+
+namespace Reflect{
+
+    enum ReflectDirection{
+        xaxis,
+        yaxis,
+        xequaly,
+        origin
+    };
+}
+
+namespace Shear{
+
+enum ShearDirection{
+    xaxis,
+    yaxis,
+    xreftoy,
+    yreftox
+};
+}
+
 // Degree to Radian Conversion
 float toRad(float deg){
     return deg*3.14159/180;
@@ -57,6 +78,82 @@ int rotate(float deg, Matrix3f triangle){
     return 1;
 }
 
+// Reflection Triangle
+int reflect(Reflect::ReflectDirection direction, Matrix3f triangle){
+    Matrix3f reflectMatrixXAxis { { 1.0f, 0,0 }, { 0,-1.0f, 0 }, { 0, 0, 1.0f } };
+    Matrix3f reflectMatrixYAxis { { -1.0f, 0,0 }, { 0,1.0f, 0 }, { 0, 0, 1.0f } };
+    Matrix3f reflectMatrixXEqualY { { -1.0f, 0,0 }, { 0,1.0f, 0 }, { 0, 0, 1.0f } };
+    Matrix3f reflectMatrixOrigin { { 0, 1.0f,0 }, { 1.0f,0, 0 }, { 0, 0, 1.0f } };
+
+
+    Matrix3f usedMatrix;
+
+    switch (direction)
+    {
+    case Reflect::ReflectDirection::xaxis:
+        usedMatrix = reflectMatrixXAxis;
+        break;
+    case Reflect::ReflectDirection::yaxis:
+        usedMatrix = reflectMatrixYAxis;
+        break;
+    case Reflect::ReflectDirection::origin:
+        usedMatrix = reflectMatrixOrigin;
+        break;
+    case Reflect::ReflectDirection::xequaly:
+        usedMatrix = reflectMatrixXEqualY;
+        break;
+    default:
+        usedMatrix = reflectMatrixOrigin;
+        break;
+    }
+    
+    for (int i = 0; i < 3; i++)
+    {
+        transformedTriangle.row(i) = usedMatrix * triangle.row(i).transpose();
+    }
+    return 1;
+}
+
+
+// Shear Triangle
+int shear(Shear::ShearDirection direction, Matrix3f triangle, float shx = 0, float shy =0, float xref = 0, float yref = 0){
+
+    Matrix3f shearMatrixXAxis { { 1.0f, shx,0 }, { 0,1.0f, 0 }, { 0, 0, 1.0f } };
+    Matrix3f shearMatrixYAxis { { 1.0f, 0,0 }, { shy,1.0f, 0 }, { 0, 0, 1.0f } };
+    Matrix3f shearMatrixXreftoY { { 1.0f, shx, -shx * yref }, { 0,1.0f, 0 }, { 0, 0, 1.0f } };
+    Matrix3f shearMatrixYreftoX { { 1.0f, 0,0 }, { shx,1.0f, -shy*xref }, { 0, 0, 1.0f } };
+
+
+    Matrix3f usedMatrix;
+
+    switch (direction)
+    {
+    case Shear::ShearDirection::xaxis:
+        usedMatrix = shearMatrixXAxis;
+        break;
+    case Shear::ShearDirection::yaxis:
+        usedMatrix = shearMatrixYAxis;
+        break;
+    case Shear::ShearDirection::xreftoy:
+        usedMatrix = shearMatrixXreftoY;
+        break;
+    case Shear::ShearDirection::yreftox:
+        usedMatrix = shearMatrixYreftoX;
+        break;
+    default:
+        usedMatrix = shearMatrixXAxis;
+        break;
+    }
+    
+    for (int i = 0; i < 3; i++)
+    {
+        transformedTriangle.row(i) = usedMatrix * triangle.row(i).transpose();
+    }
+    return 1;
+}
+
+
+
 
 void clearWindow()
 {
@@ -101,6 +198,8 @@ int main()
         translate(1.5,0.0,initialTriangle);
         rotate(180,transformedTriangle);
         scale(1.2,1.2,transformedTriangle);
+        reflect(Reflect::ReflectDirection::yaxis,transformedTriangle);
+        shear(Shear::ShearDirection::yaxis,transformedTriangle,0,1);
         drawTriangle(transformedTriangle);
         glfwSwapBuffers(window);
         glfwPollEvents();
