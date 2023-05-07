9096dcec711f7e6007ac6faf87d8b9d12b3050ae
Aadarsha Dhakal <braveboy2058@gmail.com>
2023-05-07 18:45:14 +0545

    Text Rendering Freetype Setup

---
+++ src/display_text.cpp
@@ -0,0 +1,148 @@
+#include <display_text.hpp>
+#include <GL/glew.h>
+#include <ft2build.h>
+#include <filesystem>
+#include FT_FREETYPE_H
+
+
+DisplayText::DisplayText(){
+        initialize_freetype();
+}
+
+bool DisplayText::initialize_freetype()
+{
+
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
+
+    // find path to font
+    std::string font_name = std::filesystem::path("fonts/Mistral.ttf");
+    if (font_name.empty())
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font_name" << std::endl;
+        return -1;
+    }
+
+    if (FT_New_Face(ft, font_name.c_str(), 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+
+    for (unsigned char c = 0; c < 128; c++)
+    {
+        // load character glyph
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // generate texture
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer);
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            face->glyph->advance.x};
+        Characters.insert(std::pair<char, Character>(c, character));
+    }
+
+    // Done Loading Textures from the Font
+    // Clearing FreeType's Resources Since it is of no use now
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+}
+
+Shader DisplayText::compile_text_shaders( unsigned int SCR_WIDTH = 800,unsigned int SCR_HEIGHT = 600)
+{
+    // Shader Compilation
+    glm::mat4 projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // compile and setup the shader
+    // ----------------------------
+    Shader shader("shaders/text.vs", "shaders/text.fs");
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH), 0.0f, static_cast<float>(SCR_HEIGHT));
+    shader.use();
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+    return shader;
+}
+
+void DisplayText::render_text(Shader &shader, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+    // activate corresponding render state	
+    shader.use();
+    glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+    // iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },            
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }           
+        };
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
--- src/hangman.cpp
+++ src/hangman.cpp
@@ -2,63 +2,75 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <display_text.hpp>
+#include <shader.hpp>
 
-typedef void (* GameScreen)(GLFWwindow* window);
+typedef void (*GameScreen)(GLFWwindow *window);
 
+GameScreen *gameScreen;
 
-GameScreen* gameScreen;
-
-void process_keys(GLFWwindow* window, int key, int scancode, int action,int mods){
-    if(key == GLFW_KEY_SPACE && action == GLFW_PRESS){
+void process_keys(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+    {
         std::cout << "Start The Game" << std::endl;
     }
 }
 
+int main()
+{
 
-int main(){
+    GLFWwindow *window;
 
-    GLFWwindow* window;
-
-    if(!glfwInit()){
+    if (!glfwInit())
+    {
         std::cout << "Cannot Create Window" << std::endl;
         return -1;
     }
 
-    window = glfwCreateWindow(1600,1200,"Hangman",nullptr,nullptr);
+    window = glfwCreateWindow(1600, 1200, "Hangman", nullptr, nullptr);
 
-    if(!window){
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
-    // gameScreen = startScreen();
-    if(glewInit() != GLEW_OK){
+    if (glewInit() != GLEW_OK)
+    {
         std::cout << "Error Initializing GLEW" << std::endl;
     }
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    // OpenGL State
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
     // CREATING VAO and VBA
     unsigned int buffer;
     glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER,buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
     // glBufferData(GL_ARRAY_BUFFER, 6);
-
-
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        void startScreen();
+        DisplayText* displayText = new DisplayText();
+        Shader shader = displayText->compile_text_shaders();
+        displayText->render_text(shader, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+        displayText->render_text(shader, "(C) LearnOpenGL.com", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
+
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
-        glfwSetKeyCallback(window,process_keys);
+        glfwSetKeyCallback(window, process_keys);
     }
-    
 
     glfwTerminate();
     return 0;
